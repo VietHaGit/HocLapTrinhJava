68d1bda98ea8f86317c54f0eadd814f49945c5c8
VietHaGit <jimwaltongit@gmail.com>
2022-12-08 10:53:02 +0700

    for loop ex :c

--- src/main/java/org/example/ForEx.java
+++ src/main/java/org/example/ForEx.java
@@ -60,5 +60,19 @@ public class ForEx {
 
     }
      */
+
+    //find the divisor of an integer
+    /*
+    public static void main(String[] args){
+        Scanner Sc = new Scanner(System.in);
+        int n = Sc.nextInt();
+        for (int i = 1 ; i <= n ; i ++){
+            if (n % i == 0){
+                System.out.println(i + " ");
+            }
+        }
+    }
+     */
+
     }
 
